52052c6bc689c6b656380b20a0045a978c4240ef
ts0726 <1272390200@qq.com>
2024-12-27 21:06:15 +0800

    整理git文件结构

--- KotlinSchedule/composeApp/src/androidMain/kotlin/kmp/project/schedule/App.android.kt
+++
@@ -1,9 +0,0 @@
-package kmp.project.schedule
-
-import androidx.compose.runtime.Composable
-import org.koin.compose.koinInject
-
-@Composable
-actual fun getScheduleSDK(): ScheduleSDK {
-    return koinInject()
-}
--- KotlinSchedule/composeApp/src/commonMain/kotlin/kmp/project/schedule/Platform.kt
+++
@@ -1,7 +0,0 @@
-package kmp.project.schedule
-
-interface Platform {
-    val name: String
-}
-
-expect fun getPlatform(): Platform
